a2a1dc13389fb0a9f4a72c780f34a40f054303ff
Marcus Mouyat <marcus@mouyat.com>
2022-06-08 17:43:07 +1000

    Overhauled layout and removed debug code
    This overhaul is meant as part of adding extra checks so the player does not accidentally perform one action when trying to do another, e.g. pinching when they mean to rotate. However, those checks are not fully implemented and have been omitted from this commit.

--- Assets/Scripts/Controls/TwoFingerTouchControls.cs
+++ Assets/Scripts/Controls/TwoFingerTouchControls.cs
@@ -12,37 +12,27 @@ public class TwoFingerTouchControls : MonoBehaviour
     public float pinchSensitivity;
     public float dragSensitivity;
 
-    [Header("Drag")]
-    [Range(0, 1)] public float thresholdForSameDirection = 0.8f;
+    [Range(0, 0.5f)] public float sameThreshold = 0.2f;
+    [Range(0, 0.5f)] public float oppositeThreshold = 0.2f;
+    [Range(0, 0.5f)] public float perpendicularThreshold = 0.2f;
     public UnityEvent<Vector2> onDrag;
-
-    [Header("Pinch")]
-    [Range(-1, 0)] public float thresholdForOppositeDirection = -0.8f;
     public UnityEvent<float> onPinch;
-
-    [Header("Rotate")]
     public UnityEvent<float> onRotate;
 
-    [Header("Debug")]
-    public LineRenderer debugDragLine;
-    public LineRenderer debugPinchLine;
-    public LineRenderer debugRotateLine;
-    public Graphic finger2Pressed;
-    public Text dragInfo;
-    public Text rotationInfo;
-    public Text pinchInfo;
-    float lineDistance = 1;
+    bool fingerContact1;
+    bool fingerContact2;
+    Vector2 newFingerPos1;
+    Vector2 newFingerPos2;
+    Vector2 oldFingerPos1;
+    Vector2 oldFingerPos2;
 
-    public Vector2 fingerPosition1 { get; private set; }
-    public Vector2 fingerPosition2 { get; private set; }
-    public bool fingerContact1 { get; private set; }
-    public bool fingerContact2 { get; private set; }
+    Vector2 oldDragPosition;
+    float oldPinchDistance;
+    float oldRotationAngle;
+
     public bool registeringInput => fingerContact1 && fingerContact2;
 
 
-    public float currentPinchDistance { get; private set; }
-    public float currentRotationAngle { get; private set; }
-    public Vector2 currentDragPosition { get; private set; }
 
     public float screenScale => Mathf.Min(Screen.width, Screen.height);
     
@@ -55,72 +45,80 @@ public class TwoFingerTouchControls : MonoBehaviour
 
         if (isNowRegistering == true && registeringInput == false)
         {
-            currentDragPosition = fingerPosition1 + fingerPosition2 / 2;
-            currentPinchDistance = Vector2.Distance(fingerPosition1, fingerPosition2);
-            currentRotationAngle = Vector2.SignedAngle(Vector2.up, fingerPosition2 - fingerPosition1);
+            oldDragPosition = newFingerPos1 + newFingerPos2 / 2;
+            oldPinchDistance = Vector2.Distance(newFingerPos1, newFingerPos2);
+            oldRotationAngle = Vector2.SignedAngle(Vector2.up, newFingerPos2 - newFingerPos1);
         }
         fingerContact1 = one;
         fingerContact2 = two;
-
-        finger2Pressed.gameObject.SetActive(registeringInput);
-        if (registeringInput == false)
-        {
-            pinchInfo.text = "";
-            rotationInfo.text = "";
-        }
     }
 
-    public void OnFingerPosition1(InputValue input) => UpdateInput(input.Get<Vector2>(), fingerPosition2);
-    public void OnFingerPosition2(InputValue input) => UpdateInput(fingerPosition1, input.Get<Vector2>());
-    public void UpdateInput(Vector2 newFingerPosition1, Vector2 newFingerPosition2)
+    public void OnFingerPosition1(InputValue input) => CheckInput(input.Get<Vector2>(), newFingerPos2);
+    public void OnFingerPosition2(InputValue input) => CheckInput(newFingerPos1, input.Get<Vector2>());
+    public void CheckInput(Vector2 one, Vector2 two)
     {
-        if (registeringInput)
+        // Sets most recent finger positions
+        newFingerPos1 = one;
+        newFingerPos2 = two;
+
+        bool bothFingersMoved = newFingerPos1 != oldFingerPos1 && newFingerPos2 != oldFingerPos2;
+        if (bothFingersMoved == false)
         {
-            Vector2 fingerDirection1 = newFingerPosition1 - fingerPosition1;
-            Vector2 fingerDirection2 = newFingerPosition2 - fingerPosition2;
-            float dotProduct = Vector2.Dot(fingerDirection1, fingerDirection2);
-            bool sameDirection = true;//dotProduct > thresholdForSameDirection;
-            bool oppositeDirection = true;//dotProduct < thresholdForOppositeDirection;
-
-            #region Invoke position change
-            Vector2 newDragPosition = newFingerPosition1 + newFingerPosition2 / 2;
-            if (newDragPosition != currentDragPosition && sameDirection)
-            {
-                Vector2 scaledPositionDifference = (newDragPosition - currentDragPosition) / screenScale;
-                onDrag.Invoke(scaledPositionDifference);
-                dragInfo.text = scaledPositionDifference.ToString();
-
-                currentDragPosition = newDragPosition;
-            }
-            #endregion
-
-            #region Invoke zoom change
-            float newPinchDistance = Vector2.Distance(newFingerPosition1, newFingerPosition2);
-            if (newPinchDistance != currentPinchDistance && oppositeDirection)
-            {
-                float scaledPinchDifference = (newPinchDistance - currentPinchDistance) / screenScale;
-                pinchInfo.text = scaledPinchDifference.ToString();
-                onPinch.Invoke(scaledPinchDifference);
-
-                currentPinchDistance = newPinchDistance;
-            }
-            #endregion
-
-            #region Invoke angle change
-            float newRotationAngle = Vector2.SignedAngle(Vector2.up, newFingerPosition2 - newFingerPosition1);
-            if (newRotationAngle != currentRotationAngle)
-            {
-                float scaledRotationDifference = newRotationAngle - currentRotationAngle;
-                onRotate.Invoke(scaledRotationDifference);
-                rotationInfo.text = scaledRotationDifference.ToString();
-
-                currentRotationAngle = newRotationAngle;
-            }
-            #endregion
+            // If one of the fingers has not moved, do not update old positions until both have moved
+            return;
+        }
+        else if (registeringInput == false)
+        {
+            // Unless only one finger is pressed, because this means the player has not started a two-finger action yet
+            oldFingerPos1 = newFingerPos1;
+            oldFingerPos2 = newFingerPos2;
+            return;
         }
 
-        fingerPosition1 = newFingerPosition1;
-        fingerPosition2 = newFingerPosition2;
+        RegisterNewInput(); // If player is moving both fingers, register new input
     }
 
+    void RegisterNewInput()
+    {
+        bool dragging = true;//sameDirection; // If fingers are moving in the same direction, player is dragging
+        bool pinching = true;//oppositeDirection && !perp1 && !perp2; // If opposite directions but not perpendicular, player is pinching/stretching
+        bool rotating = true;//oppositeDirection;// && perp1 && perp2; // If opposite and perpendicular, player is performing a rotation
+
+        #region Invoke position change
+        Vector2 newDragPosition = newFingerPos1 + newFingerPos2 / 2;
+        if (newDragPosition != oldDragPosition && dragging)
+        {
+            Vector2 scaledPositionDifference = (newDragPosition - oldDragPosition) / screenScale;
+            onDrag.Invoke(scaledPositionDifference);
+
+            oldDragPosition = newDragPosition;
+        }
+        #endregion
+
+        #region Invoke zoom change
+        float newPinchDistance = Vector2.Distance(newFingerPos1, newFingerPos2);
+        if (newPinchDistance != oldPinchDistance && pinching)
+        {
+            float scaledPinchDifference = (newPinchDistance - oldPinchDistance) / screenScale;
+            onPinch.Invoke(scaledPinchDifference);
+
+            oldPinchDistance = newPinchDistance;
+        }
+        #endregion
+
+        #region Invoke angle change
+        float newRotationAngle = Vector2.SignedAngle(Vector2.up, newFingerPos2 - newFingerPos1);
+        if (newRotationAngle != oldRotationAngle && rotating)
+        {
+            float scaledRotationDifference = newRotationAngle - oldRotationAngle;
+            onRotate.Invoke(scaledRotationDifference);
+
+            oldRotationAngle = newRotationAngle;
+        }
+        #endregion
+
+        // Overwrite old inputs to match the current ones, so differences can be accurately checked next time
+        oldFingerPos1 = newFingerPos1;
+        oldFingerPos2 = newFingerPos2;
+    }
 }
