167acab1444974bd34259c8936f496281a9eb974
Marcus Mouyat <marcus@mouyat.com>
2022-06-14 18:19:01 +1000

    Object viewing code for rotating on perpendicular axes
    This code uses Transform.RotateAround to ensure that the object rotates the same way relative to the camera and screen, regardless of its current rotation axis.

--- Assets/Scripts/Controls/ObjectView.cs
+++ Assets/Scripts/Controls/ObjectView.cs
@@ -7,4 +7,17 @@ public class ObjectView : MonoBehaviour
     public Transform viewedObject;
     public Camera viewingCamera;
 
+    [Header("Touch control info")]
+    public TwoFingerTouchControls twoFingerControls;
+    public float rotationSensitivity = 90;
+    private void Awake()
+    {
+        twoFingerControls?.onDrag.AddListener(RotateOnPerpendicularAxes);
+    }
+    public void RotateOnPerpendicularAxes(Vector2 input)
+    {
+        input = rotationSensitivity * input;
+        viewedObject.RotateAround(viewedObject.transform.position, viewingCamera.transform.up, -input.x);
+        viewedObject.RotateAround(viewedObject.transform.position, viewingCamera.transform.right, input.y);
+    }
 }
