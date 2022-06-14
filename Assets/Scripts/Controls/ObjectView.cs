f44fb1596c8f30c69a9005c32eeb2bd060d4559a
Marcus Mouyat <marcus@mouyat.com>
2022-06-14 18:22:11 +1000

    Pinch-to-zoom code in ObjectView
    This function will move the object towards or away from the camera, with an additional 'sanity check' function to ensure the object is not too close or far away.

--- Assets/Scripts/Controls/ObjectView.cs
+++ Assets/Scripts/Controls/ObjectView.cs
@@ -10,9 +10,14 @@ public class ObjectView : MonoBehaviour
     [Header("Touch control info")]
     public TwoFingerTouchControls twoFingerControls;
     public float rotationSensitivity = 90;
+    public float zoomSensitivity = 10;
+    public float minZoomDistance = 2;
+    public float maxZoomDistance = 20;
+
     private void Awake()
     {
         twoFingerControls?.onDrag.AddListener(RotateOnPerpendicularAxes);
+        twoFingerControls?.onPinch.AddListener(Zoom);
         twoFingerControls?.onRotate.AddListener(RotateOnCameraAxis);
     }
     public void RotateOnPerpendicularAxes(Vector2 input)
@@ -24,5 +29,20 @@ public class ObjectView : MonoBehaviour
     public void RotateOnCameraAxis(float angle)
     {
         viewedObject.RotateAround(viewedObject.transform.position, viewedObject.position - viewingCamera.transform.position, angle);
+    }
+    public void Zoom(float distance)
+    {
+        float extraDistance = -distance * zoomSensitivity;
+
+        viewedObject.transform.position += extraDistance * viewingCamera.transform.forward;
+
+        DistanceSanityCheck();
+    }
+
+    void DistanceSanityCheck()
+    {
+        Vector3 offsetFromCamera = viewedObject.transform.position - viewingCamera.transform.position;
+        offsetFromCamera = Mathf.Clamp(offsetFromCamera.magnitude, minZoomDistance, maxZoomDistance) * offsetFromCamera.normalized;
+        viewedObject.transform.position = viewingCamera.transform.position + offsetFromCamera;
     }
 }
