6dec1cb9736531d3daa616887b4075dfdb303bf1
Marcus Mouyat <marcus@mouyat.com>
2022-06-14 18:17:10 +1000

    Started creating ObjectView class to test touch controls by manipulating an object

---
+++ Assets/Scripts/Controls/ObjectView.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectView : MonoBehaviour
+{
+    public Transform viewedObject;
+    public Camera viewingCamera;
+
+}
