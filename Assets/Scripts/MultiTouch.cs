4365ec06ce91fec71512f75d775daf56b13c471a
Marcus Mouyat <marcus@mouyat.com>
2022-07-14 11:42:30 +1000

    MultiTouch class for easily obtaining and tracking multiple touch values
    This class allows multiple touch inputs to be obtained using the new input system, without the hassle of setting up an InputActionAsset or a PlayerInput.
    This script will set up multiple InputActions to register touch inputs – one for registering contact and one for the finger position, multiplied by the supported number of touches. By automating it, I can easily set up a large number of them to support as many separate touches as the device will allow (or 10, as allowed by the input system), which would be much more obnoxious to set up manually due to the time expended, the separate functions having to be set up for each finger, and the possibility of making errors and not assigning variables and interactions properly.
    The script also seamlessly tracks the number of active touches and displays their positions in an array, without needing to worry about which specific ones are being registered in the array, which makes it much easier to execute two-finger commands without worrying about their current fingers not being counted because they aren’t technically the first and second fingers. If specific touch inputs still need to be obtained, the InputActions can still be accessed through a public property.
    I previously experimented with a version of this that used EventSystems interfaces – IPointerDownHandler, IPointerUpHandler, and IDragHandler. However, due to a quirk in the way touches are added and removed during that system, it was possible for the player to press or release multiple fingers in a single frame, and have only one change be registered. This resulted in phantom touches that the code thought were still being pressed. This new system swiftly avoids that problem because each touch is being tracked separately.
    Overall, I’m quite pleased with it!

---
+++ Assets/Scripts/MultiTouch.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Events;
+
+public class MultiTouch : MonoBehaviour
+{
+    public UnityEvent<MultiTouch> onValuesChanged;
+    public UnityEngine.UI.Text debugWindow;
+    
+    int touchCount = 10;
+
+    #region Properties
+    public InputAction[] contactActions { get; private set; }
+    public InputAction[] positionActions { get; private set; }
+    public int contacts { get; private set; }
+    public Vector2[] positions { get; private set; }
+    #endregion
+
+    private void Awake() => SetupActions();
+
+    /// <summary>
+    /// Sets up the InputActions for positions and contacts
+    /// </summary>
+    void SetupActions()
+    {
+        positions = new Vector2[touchCount];
+        contactActions = new InputAction[touchCount];
+        positionActions = new InputAction[touchCount];
+        for (int i = 0; i < touchCount; i++)
+        {
+            // Set up contact action
+            contactActions[i] = new InputAction(
+                name: "Finger Contact #" + (i + 1),
+                type: InputActionType.Button,
+                binding: "<Touchscreen>/touch" + i + "/press",
+                interactions: "Press(behavior=2)",
+                expectedControlType: "Touch"
+                );
+
+            // Set up position action
+            positionActions[i] = new InputAction(
+                name: "Finger Position #" + (i + 1),
+                type: InputActionType.Value,
+                binding: "<Touchscreen>/touch" + i + "/position",
+                expectedControlType: "Touch"
+                );
+
+            // Add listeners to update positions and count when inputs change
+            contactActions[i].performed += (_) => OnValuesChanged();
+            positionActions[i].performed += (_) => OnValuesChanged();
+        }
+    }
+    /// <summary>
+    /// Runs when the contacts or positions change.
+    /// </summary>
+    void OnValuesChanged()
+    {
+        int touchNumber = 0;
+        for (int i = 0; i < touchCount; i++)
+        {
+            if (contactActions[i].IsPressed())
+            {
+                positions[touchNumber] = positionActions[i].ReadValue<Vector2>();
+                touchNumber++;
+            }
+        }
+        contacts = touchNumber;
+        onValuesChanged.Invoke(this);
+
+        #region Debug!
+        string message = "Frame: " + Time.frameCount + "\nTouch count: " + contacts;
+        for (int i = 0; i < contacts; i++)
+        {
+            message += "\nPosition " + (i + 1) + ": " + positions[i];
+        }
+        Debug.Log(message);
+        if (debugWindow != null) debugWindow.text = message;
+        #endregion
+    }
+
+    #region Enable/disable InputActions to save processing power
+    private void OnEnable() => SetInputActiveState(true);
+    private void OnDisable() => SetInputActiveState(false);
+    void SetInputActiveState(bool active)
+    {
+        for (int i = 0; i < touchCount; i++)
+        {
+            if (active)
+            {
+                contactActions[i].Enable();
+                positionActions[i].Enable();
+            }
+            else
+            {
+                contactActions[i].Disable();
+                positionActions[i].Disable();
+            }
+        }
+    }
+    #endregion
+}
